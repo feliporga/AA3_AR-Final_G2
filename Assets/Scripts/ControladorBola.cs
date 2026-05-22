a655c792f0ba68cab0ea90c79a14d990f4f00572
Felip Orga <felip.orga@estudiant.enti.cat>
2026-05-22 13:38:27 +0200

    added virtual putter

--- Assets/Scripts/ControladorBola.cs
+++ Assets/Scripts/ControladorBola.cs
@@ -8,23 +8,29 @@ public class ControladorBola : MonoBehaviour
     public float fuerzaGolpe = 2.5f;
     private Rigidbody rb;
     private Camera camaraPrincipal;
-    //[SerializeField] private InputActionReference space;
+    [SerializeField] private InputActionReference space;
     [SerializeField] private Vector3 direction = Vector3.forward;
+
+    [SerializeField] private Transform putterVisual;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         camaraPrincipal = Camera.main;
 
-        
+        if (putterVisual == null)
+        {
+            putterVisual = transform.Find("PivotePutter");
+        }
+
         var managerGiro = FindFirstObjectByType<HerramientaGiroscopio.ManagerGiroscopio>();
         if (managerGiro != null)
         {
             managerGiro.AlSaltar.AddListener(GolpearBola);
         }
 
-        //space.action.performed += TestPush;
-        //space.action.Enable();
+        space.action.performed += TestPush;
+        space.action.Enable();
     }
 
     private void TestPush(InputAction.CallbackContext obj)
@@ -40,5 +46,23 @@ public class ControladorBola : MonoBehaviour
         direccionGolpe.Normalize();
 
         rb.AddForce(direccionGolpe * fuerzaGolpe, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        // Orientar el putter hacia la dirección de la cámara
+        if (putterVisual != null && camaraPrincipal != null)
+        {
+            Vector3 direccionCamara = camaraPrincipal.transform.forward;
+
+            direccionCamara.y = 0;
+
+            if (direccionCamara != Vector3.zero)
+            {
+                Quaternion rotacionObjetivo = Quaternion.LookRotation(direccionCamara);
+
+                putterVisual.rotation = rotacionObjetivo;
+            }
+        }
     }
 }
