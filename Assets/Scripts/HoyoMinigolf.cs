e4b3bb4ab6f1e589d9e7b4d74a37d9fa8c19c6ce
izvn <124562397+izvn@users.noreply.github.com>
2026-05-31 18:27:02 +0200

    Fix

--- Assets/Scripts/HoyoMinigolf.cs
+++ Assets/Scripts/HoyoMinigolf.cs
@@ -4,8 +4,7 @@ using TMPro;
 public class HoyoMinigolf : MonoBehaviour
 {
     public TextMeshProUGUI textoVictoria;
-
-    
+    private Collider miCollider;
 
     private void Start()
     {
@@ -15,22 +14,32 @@ public class HoyoMinigolf : MonoBehaviour
         {
             textoVictoria = objetoTexto.GetComponent<TextMeshProUGUI>();
         }
+
+        miCollider = GetComponent<Collider>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (textoVictoria != null)
+            if (miCollider != null)
             {
-                textoVictoria.text = "YOU WON";
-                textoVictoria.gameObject.SetActive(true);
+                Vector3 posicionDentro = miCollider.ClosestPoint(other.transform.position);
+                other.transform.position = posicionDentro;
             }
 
             Rigidbody rbBola = other.GetComponent<Rigidbody>();
             if (rbBola != null)
             {
                 rbBola.linearVelocity = Vector3.zero;
+                rbBola.angularVelocity = Vector3.zero;
+                rbBola.isKinematic = true;
+            }
+
+            if (textoVictoria != null)
+            {
+                textoVictoria.text = "YOU WON";
+                textoVictoria.gameObject.SetActive(true);
             }
         }
     }
